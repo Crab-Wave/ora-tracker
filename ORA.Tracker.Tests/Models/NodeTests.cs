7691d9061e8148340fbfc1e5b8e8374016a693eb
TrAyZeN <1810leo@gmail.com>
2020-02-28 15:52:37 +0100

    :white_check_mark: Add constructor test

--- ORA.Tracker.Tests/Models/NodeTests.cs
+++ ORA.Tracker.Tests/Models/NodeTests.cs
@@ -11,6 +11,18 @@ namespace ORA.Tracker.Tests.Models
         [InlineData("", "")]
         [InlineData("2666b73d-059f-44c6-b29d-779b45540fa1", "127.0.0.1")]
         [InlineData("265d0721-850d-485e-8c9b-98e036dfb739", "8.8.8.8")]
+        public void WhenCreatingNode_ShouldHaveMatchingFields(string uid, string current_ip)
+        {
+            var testee = new Node(uid, current_ip);
+
+            testee.uid.Should().Be(uid);
+            testee.current_ip.Shoud().Be(current_ip);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("2666b73d-059f-44c6-b29d-779b45540fa1", "127.0.0.1")]
+        [InlineData("265d0721-850d-485e-8c9b-98e036dfb739", "8.8.8.8")]
         public void WhenConvertingToString_ShouldMatch(string uid, string current_ip)
         {
             var testee = new Node(uid, current_ip);
