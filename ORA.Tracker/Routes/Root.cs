361119ad71a97d1242a777dda2e276b5c0f35c19
TrAyZeN <1810leo@gmail.com>
2020-03-06 18:31:45 +0100

    :zap: Store welcome message

--- ORA.Tracker/Routes/Root.cs
+++ ORA.Tracker/Routes/Root.cs
@@ -5,10 +5,11 @@ namespace ORA.Tracker.Routes
 {
     public class Root : Route
     {
+        private static readonly byte[] welcomeMessage = Encoding.UTF8.GetBytes("Hey welcome to '/'");
         public Root()
             : base("/") { }
 
         protected override byte[] get(HttpListenerRequest request, HttpListenerResponse response)
-            => Encoding.UTF8.GetBytes("Hey welcome to '/'");
+            => welcomeMessage;
     }
 }
