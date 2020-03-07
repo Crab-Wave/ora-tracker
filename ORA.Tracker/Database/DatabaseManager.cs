fa383d807898aeb3aebc912842e3c87f04d54abf
TrAyZeN <1810leo@gmail.com>
2020-03-07 19:29:34 +0100

    :sparkles: Add getAll method

--- ORA.Tracker/Database/DatabaseManager.cs
+++ ORA.Tracker/Database/DatabaseManager.cs
@@ -1,5 +1,7 @@
 using System;
+using System.IO;
 using System.Text;
+using System.Text.Json;
 using LevelDB;
 
 using ORA.Tracker.Models;
@@ -42,6 +44,49 @@ namespace ORA.Tracker.Database
                 throw new Exception("DatabaseManager is not initialized");
 
             return Encoding.UTF8.GetBytes(database.Get(key));
+        }
+
+        public static byte[] GetAll()
+        {
+            if (database == null)
+                throw new Exception("DatabaseManager is not initialized");
+
+            var stream = new MemoryStream();
+            var writer = new Utf8JsonWriter(stream, new JsonWriterOptions { Indented = true });
+            var iterator = database.CreateIterator();
+            Cluster cluster;
+
+            writer.WriteStartArray();
+            for (iterator.SeekToFirst(); iterator.IsValid(); iterator.Next())
+            {
+                cluster = JsonSerializer.Deserialize<Cluster>(database.Get(iterator.KeyAsString()));
+                writer.WriteStartObject();
+
+                writer.WriteString("id", cluster.id.ToString());
+                writer.WriteString("name", cluster.name);
+                writer.WriteString("owner", cluster.owner.ToString());
+
+                writer.WriteStartArray("members");
+                foreach (string member in cluster.members)
+                    writer.WriteStringValue(member);
+                writer.WriteEndArray();
+
+                writer.WriteStartArray("admins");
+                foreach (string admin in cluster.admins)
+                    writer.WriteStringValue(admin);
+                writer.WriteEndArray();
+
+                writer.WriteStartArray("files");
+                foreach (string file in cluster.files)
+                    writer.WriteStringValue(file);
+                writer.WriteEndArray();
+
+                writer.WriteEndObject();
+            }
+            writer.WriteEndArray();
+            writer.Flush();
+
+            return stream.ToArray();
         }
 
         public static void Delete(string key)
