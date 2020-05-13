604532a59ede73b25cdf19ec2663827c5388c772
TrAyZeN <1810leo@gmail.com>
2020-05-13 17:40:35 +0200

    :clown_face: Generate context with url parameters

--- ORA.Tracker.Tests/Integration/Utils/MockupListener.cs
+++ ORA.Tracker.Tests/Integration/Utils/MockupListener.cs
@@ -1,19 +1,21 @@
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
+using ORA.Tracker.Http;
+
 namespace ORA.Tracker.Tests.Integration.Utils
 {
     internal class MockupListener
     {
-        private HttpClient client;
+        private static readonly HttpClient client = new HttpClient();
         private string listener_uri;
         private HttpListener listener;
 
         public MockupListener(int port)
         {
-            this.client = new HttpClient();
             this.listener_uri = $"http://localhost:{port}/";
             this.listener = new HttpListener() { Prefixes = { listener_uri } };
 
@@ -21,22 +23,37 @@ namespace ORA.Tracker.Tests.Integration.Utils
                 this.listener.Start();
         }
 
-        public async Task<HttpListenerContext> GenerateContext(string path, HttpMethod method, byte[] bodyContent, string credentials)
+        public async Task<(HttpRequest, HttpListenerResponse)> GetContext(string path, Dictionary<string, string> urlParameters,
+            HttpMethod method, byte[] bodyContent, string credentials)
         {
             _ = this.makeRequest(method, this.listener_uri + path, bodyContent, credentials);
             HttpListenerContext context = await this.listener.GetContextAsync();
 
-            return context;
+            return (new HttpRequest(context.Request, urlParameters), context.Response);
         }
 
-        public async Task<HttpListenerContext> GenerateContext(string path, HttpMethod method) =>
-            await GenerateContext(path, method, null, null);
+        public async Task<(HttpRequest, HttpListenerResponse)> GetContext(string path, HttpMethod method, byte[] bodyContent, string credentials)
+        {
+            _ = this.makeRequest(method, this.listener_uri + path, bodyContent, credentials);
+            HttpListenerContext context = await this.listener.GetContextAsync();
 
-        public async Task<HttpListenerContext> GenerateContext(string path, HttpMethod method, byte[] bodyContent) =>
-            await GenerateContext(path, method, bodyContent, null);
+            return (new HttpRequest(context.Request, new Dictionary<string, string>()), context.Response);
+        }
 
-        public async Task<HttpListenerContext> GenerateContext(string path, HttpMethod method, string credentials) =>
-            await GenerateContext(path, method, null, credentials);
+        public async Task<(HttpRequest, HttpListenerResponse)> GetContext(string path, HttpMethod method) =>
+            await GetContext(path, method, null, null);
+
+        public async Task<(HttpRequest, HttpListenerResponse)> GetContext(string path, Dictionary<string, string> urlParameters, HttpMethod method) =>
+            await GetContext(path, urlParameters, method, null, null);
+
+        public async Task<(HttpRequest, HttpListenerResponse)> GetContext(string path, Dictionary<string, string> urlParameters, HttpMethod method, string credentials) =>
+            await GetContext(path, urlParameters, method, null, credentials);
+
+        public async Task<(HttpRequest, HttpListenerResponse)> GetContext(string path, HttpMethod method, byte[] bodyContent) =>
+            await GetContext(path, method, bodyContent, null);
+
+        public async Task<(HttpRequest, HttpListenerResponse)> GetContext(string path, HttpMethod method, string credentials) =>
+            await GetContext(path, method, null, credentials);
 
         private async Task<HttpResponseMessage> makeRequest(HttpMethod method, string uri, byte[] bodyContent, string credentials)
         {
@@ -47,7 +64,7 @@ namespace ORA.Tracker.Tests.Integration.Utils
             if (credentials != null)
                 message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", credentials);
 
-            return await this.client.SendAsync(message);
+            return await client.SendAsync(message);
         }
     }
 }
