2c0c60f7fa9d56f3a7d739237550d194324475f3
TrAyZeN <1810leo@gmail.com>
2020-05-21 17:27:08 +0200

    :white_check_mark: Add admins tests

--- ORA.Tracker.Tests/Integration/Routes/AdminsTests.cs
+++ ORA.Tracker.Tests/Integration/Routes/AdminsTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net.Http;
 using Xunit;
 using FluentAssertions;
+using Xunit.Abstractions;
 
 using ORA.Tracker.Models;
 using ORA.Tracker.Services;
@@ -89,6 +90,220 @@ namespace ORA.Tracker.Routes.Tests.Integration
 
             response.StatusCode.Should().Be(403);
             response.Content.ReadAsStringAsync().Result.Should().Be(expectedResponseContent);
+        }
+
+        [Fact]
+        public async void Get_WhenMemberOfCluster_ShouldRespondWithListOfAdmins()
+        {
+            var cluster = new Cluster("testcluster", services.TokenManager.GetIdFromToken(this.token), "ownername");
+            services.ClusterManager.Put(cluster);
+
+            var request = new MockupRouterRequest(HttpMethod.Get, $"/clusters/{cluster.id.ToString()}/admins")
+            {
+                Credentials = this.token
+            };
+            var response = await router.GetResponseOf(request);
+
+            response.StatusCode.Should().Be(200);
+            response.Content.ReadAsByteArrayAsync().Result.Should().BeEquivalentTo(cluster.SerializeAdmins());
+        }
+
+        [Fact]
+        public async void Post_WhenMissingCredentials_ShouldRespondWithBadRequest()
+        {
+            string expectedResponseContent = new Error("Missing credentials").ToString();
+
+            var response = await router.GetResponseOf(HttpMethod.Post, "/clusters/id/admins?id=id");
+
+            response.StatusCode.Should().Be(400);
+            response.Content.ReadAsStringAsync().Result.Should().Be(expectedResponseContent);
+        }
+
+        [Fact]
+        public async void Post_WhenInvalid_ShouldRespondWithBadRequest()
+        {
+            string expectedResponseContent = new Error($"Missing query parameter id").ToString();
+
+            var request = new MockupRouterRequest(HttpMethod.Post, "/clusters/id/admins")
+            {
+                Credentials = this.token
+            };
+            var response = await router.GetResponseOf(request);
+
+            response.StatusCode.Should().Be(400);
+            response.Content.ReadAsStringAsync().Result.Should().Be(expectedResponseContent);
+        }
+
+        [Fact]
+        public async void Post_WhenInvalidClusterId_ShouldRespondWithNotFound()
+        {
+            string expectedResponseContent = new Error("Invalid Cluster id").ToString();
+            string invalidClusterId = "invalidclusterid";
+            var request = new MockupRouterRequest(HttpMethod.Post, $"/clusters/{invalidClusterId}/admins?id=1234")
+            {
+                Credentials = this.token
+            };
+            var response = await router.GetResponseOf(request);
+
+            response.StatusCode.Should().Be(404);
+            response.Content.ReadAsStringAsync().Result.Should().Be(expectedResponseContent);
+        }
+
+        [Fact]
+        public async void Post_WhenNotOwnerOfCluster_ShouldRespondWithForbidden()
+        {
+            string expectedResponseContent = new Error("Unauthorized action").ToString();
+
+            string ownerId = Guid.NewGuid().ToString();
+            string token = services.TokenManager.NewToken();
+            services.TokenManager.RegisterToken(ownerId, token);
+            var cluster = new Cluster("testcluster", ownerId, "ownername");
+            cluster.members.Add(Guid.NewGuid().ToString(), this.token);
+            services.ClusterManager.Put(cluster);
+
+            var request = new MockupRouterRequest(HttpMethod.Post, $"/clusters/{cluster.id.ToString()}/admins?id=123")
+            {
+                Credentials = this.token
+            };
+            var response = await router.GetResponseOf(request);
+
+            response.StatusCode.Should().Be(403);
+            response.Content.ReadAsStringAsync().Result.Should().Be(expectedResponseContent);
+        }
+
+        [Fact]
+        public async void Post_WhenIdIsNotMemberOfCluster_ShouldRespondWithBadRequest()
+        {
+            string expectedResponseContent = new Error("id does not correspond to a cluster member").ToString();
+
+            var cluster = new Cluster("testcluster", services.TokenManager.GetIdFromToken(this.token), "ownername");
+            services.ClusterManager.Put(cluster);
+
+            var request = new MockupRouterRequest(HttpMethod.Post, $"/clusters/{cluster.id.ToString()}/admins?id=123")
+            {
+                Credentials = this.token
+            };
+            var response = await router.GetResponseOf(request);
+
+            response.StatusCode.Should().Be(400);
+            response.Content.ReadAsStringAsync().Result.Should().Be(expectedResponseContent);
+        }
+
+        [Fact]
+        public async void Post_WhenOwner_ShouldRespondWithOK()
+        {
+            string memberId = "memberid";
+            var cluster = new Cluster("testcluster", services.TokenManager.GetIdFromToken(this.token), "ownername");
+            cluster.members.Add(memberId, "memberToken");
+            services.ClusterManager.Put(cluster);
+
+            var request = new MockupRouterRequest(HttpMethod.Post, $"/clusters/{cluster.id.ToString()}/admins?id={memberId}")
+            {
+                Credentials = this.token
+            };
+            var response = await router.GetResponseOf(request);
+
+            response.StatusCode.Should().Be(200);
+            response.Content.ReadAsStringAsync().Result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async void Delete_WhenMissingCredentials_ShouldRespondWithBadRequest()
+        {
+            string expectedResponseContent = new Error("Missing credentials").ToString();
+
+            var response = await router.GetResponseOf(HttpMethod.Delete, "/clusters/id/admins?id=id");
+
+            response.StatusCode.Should().Be(400);
+            response.Content.ReadAsStringAsync().Result.Should().Be(expectedResponseContent);
+        }
+
+        [Fact]
+        public async void Delete_WhenInvalid_ShouldRespondWithBadRequest()
+        {
+            string expectedResponseContent = new Error($"Missing query parameter id").ToString();
+
+            var request = new MockupRouterRequest(HttpMethod.Delete, "/clusters/id/admins")
+            {
+                Credentials = this.token
+            };
+            var response = await router.GetResponseOf(request);
+
+            response.StatusCode.Should().Be(400);
+            response.Content.ReadAsStringAsync().Result.Should().Be(expectedResponseContent);
+        }
+
+        [Fact]
+        public async void Delete_WhenInvalidClusterId_ShouldRespondWithNotFound()
+        {
+            string expectedResponseContent = new Error("Invalid Cluster id").ToString();
+            string invalidClusterId = "invalidclusterid";
+            var request = new MockupRouterRequest(HttpMethod.Delete, $"/clusters/{invalidClusterId}/admins?id=1234")
+            {
+                Credentials = this.token
+            };
+            var response = await router.GetResponseOf(request);
+
+            response.StatusCode.Should().Be(404);
+            response.Content.ReadAsStringAsync().Result.Should().Be(expectedResponseContent);
+        }
+
+        [Fact]
+        public async void Delete_WhenNotOwnerOfCluster_ShouldRespondWithForbidden()
+        {
+            string expectedResponseContent = new Error("Unauthorized action").ToString();
+
+            string ownerId = Guid.NewGuid().ToString();
+            string token = services.TokenManager.NewToken();
+            services.TokenManager.RegisterToken(ownerId, token);
+            var cluster = new Cluster("testcluster", ownerId, "ownername");
+            cluster.members.Add(Guid.NewGuid().ToString(), this.token);
+            services.ClusterManager.Put(cluster);
+
+            var request = new MockupRouterRequest(HttpMethod.Delete, $"/clusters/{cluster.id.ToString()}/admins?id=123")
+            {
+                Credentials = this.token
+            };
+            var response = await router.GetResponseOf(request);
+
+            response.StatusCode.Should().Be(403);
+            response.Content.ReadAsStringAsync().Result.Should().Be(expectedResponseContent);
+        }
+
+        [Fact]
+        public async void Delete_WhenIdIsNotAdminOfCluster_ShouldRespondWithBadRequest()
+        {
+            string expectedResponseContent = new Error("id does not correspond to a cluster admin").ToString();
+
+            var cluster = new Cluster("testcluster", services.TokenManager.GetIdFromToken(this.token), "ownername");
+            services.ClusterManager.Put(cluster);
+
+            var request = new MockupRouterRequest(HttpMethod.Delete, $"/clusters/{cluster.id.ToString()}/admins?id=123")
+            {
+                Credentials = this.token
+            };
+            var response = await router.GetResponseOf(request);
+
+            response.StatusCode.Should().Be(400);
+            response.Content.ReadAsStringAsync().Result.Should().Be(expectedResponseContent);
+        }
+
+        [Fact]
+        public async void Delete_WhenOwner_ShouldRespondWithOK()
+        {
+            string memberId = "memberid";
+            var cluster = new Cluster("testcluster", services.TokenManager.GetIdFromToken(this.token), "ownername");
+            cluster.admins.Add(memberId);
+            services.ClusterManager.Put(cluster);
+
+            var request = new MockupRouterRequest(HttpMethod.Delete, $"/clusters/{cluster.id.ToString()}/admins?id={memberId}")
+            {
+                Credentials = this.token
+            };
+            var response = await router.GetResponseOf(request);
+
+            response.StatusCode.Should().Be(200);
+            response.Content.ReadAsStringAsync().Result.Should().BeEmpty();
         }
 
         [Fact]
